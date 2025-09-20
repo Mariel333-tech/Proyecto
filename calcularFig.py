6dcbcb8247996c1c0e49ad9787ba50b0e171c2d3
Mariel Ramírez <maramislas0407@gmail.com>
2025-09-20 16:32:39 -0600

    5to. commit ya con modificación

--- calcularFig.py
+++ calcularFig.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-from figuras import rectangulo
+from figuras import rectangulo, triangulo, circulo
 
 df = pd.read_csv("figuras.csv")
 print("El archivo ha sido leido")
@@ -9,7 +9,22 @@ print("El archivo ha sido leido")
 for index, row in df.iterrows():
 	print(f"Fila {index}: FIGURA={row['FIGURA']}, MEDIDA1={row['MEDIDA1']}, MEDIDA2={row['MEDIDA2']}")
 
+for index, row in df.iterrows():
+	figura = row['FIGURA']
+	m1 = row['MEDIDA1']
+	m2 = row['MEDIDA2']
+
+if figura == 'r':  # rectángulo
+	area, perimetro = rectangulo(m1, m2)
+	print(f"Fila {index}: Rectángulo base={m1}, altura={m2} -> Área={area:.2f}, Perímetro={perimetro:.2f}")
+
+elif figura == 't':  # triángulo
+	area, perimetro = triangulo(m1, m2)
+	print(f"Fila {index}: Triángulo base={m1}, altura={m2} -> Área={area:.2f}, Perímetro={perimetro:.2f}")
+
+elif figura == 'c':  # círculo
+	area, perimetro = circulo(m1)  # solo usa MEDIDA1 como radio
+	print(f"Fila {index}: Círculo radio={m1} -> Área={area:.2f}, Perímetro={perimetro:.2f}")
 
 
-a = rectangulo(10,5)
-print(a)
+
--- figuras.py
+++ figuras.py
@@ -1,4 +1,16 @@
-def rectangulo(base , altura)
+import math
+def rectangulo(base , altura):
 	area= base * altura
-	perimetro= 2 * (basse + altura)
+	perimetro= 2 * (base + altura)
 	return area, perimetro
+def triangulo(base, altura):
+    area = (base * altura) / 2
+    lado = math.sqrt((base / 2)**2 + altura**2)
+    perimetro = base + 2 * lado
+    return area, perimetro
+def circulo(radio):
+    area = math.pi * radio**2
+    perimetro = 2 * math.pi * radio
+    return area, perimetro
+
+
